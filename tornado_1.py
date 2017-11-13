4e34b84d91890e7bc4c041f762c7d9e6e829cca2
Olya <johndoe@example.com>
2017-11-13 17:02:55 +0200

    test tornado redis

--- tornado_1.py
+++ tornado_1.py
@@ -17,7 +17,9 @@ c.connect()
 class MainHandler(tornado.web.RequestHandler):
 
     @tornado.gen.engine
-    async def get(self):
+    @tornado.web.asynchronous
+    def get(self):
+    # async def get(self):
         # # try:
         #     http = AsyncHTTPClient()
         #     r = await http.fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
@@ -34,11 +36,27 @@ class MainHandler(tornado.web.RequestHandler):
         #         self.write("Try again")
 
         # try:
+        print('tetst')
         http = AsyncHTTPClient()
-        foo = await tornado.gen.Task(c.get, "foo")
-        print(foo)
+        bit = yield tornado.gen.Task(c.get, "bit")
+        print(bit)
+
+        r = yield tornado.http.fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
+
+        if r.reason == 'OK':
+            print(r.body)
+            date = json_decode(r.body)
+            print(date['bpi']['USD']['rate_float'])
+            res = date['bpi']['USD']['rate_float']
+            # self.write("Bitcoin =  %s" % res)
+            print(res)
+
+            with c.pipeline() as pipe:
+                pipe.set('foo', res, 12 * 60 * 60)
+                yield tornado.gen.Task(pipe.execute)
+
         self.set_header('Content-Type', 'text/html')
-        self.render("template.html", title="Simple demo", foo=foo)
+        self.render("template.html", title="Simple demo", bit=bit)
 
         '''if r.reason == 'OK':
             print(r.body)
@@ -51,12 +69,41 @@ class MainHandler(tornado.web.RequestHandler):
             self.write("Try again")'''
 
 
+@tornado.gen.engine
+async def create_test_data():
+    c = tornadoredis.Client()
+    r = await tornado.http.fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
+
+    if r.reason == 'OK':
+        print(r.body)
+        date = json_decode(r.body)
+        print(date['bpi']['USD']['rate_float'])
+        res = date['bpi']['USD']['rate_float']
+        # self.write("Bitcoin =  %s" % res)
+        print(res)
+
+        with c.pipeline() as pipe:
+            pipe.set('foo', res, 12 * 60 * 60)
+            await tornado.gen.Task(pipe.execute)
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
 
+application = tornado.web.Application([
+    (r'/', MainHandler),
+])
+
 if __name__ == "__main__":
-    app = make_app()
+    # create_test_data()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    print('Demo is runing at 0.0.0.0:8888\nQuit the demo with CONTROL-C')
+    tornado.ioloop.IOLoop.instance().start()
+
+
+    '''app = make_app()
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.current().start()'''
